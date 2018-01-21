c650c82d8ffa5db40c410549ee9c732894482636
Jake <jesmith24@comcast.net>
2018-01-21 02:03:05 -0800

    quality of life changes removed for testing

--- scripts/vscripts/lina.lua
+++ scripts/vscripts/lina.lua
@@ -4,12 +4,12 @@ function ZeroManaOnSpawn( event )
         local hero = event.caster
         Timers:CreateTimer(.01, function()
         -- Set Mana to 0 on created
-        -- hero:SetMana(0)
+        hero:SetMana(0)
         hero:SetHealth(20000)
     end)
  end
 
  function ChargeOrbPickup( event )
     local hero = event.caster
-    hero:GiveMana(20)
+    hero:GiveMana(10)
  end
